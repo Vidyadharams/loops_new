d8e9bdc7b19ce09d4b043cb15926935dffd4d711
Vidyadhara M S <vidyadharams179@gmail.com>
2024-07-08 08:35:15 +0530

    Add files via upload

---
+++ dowhile2.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class dowhile2 {
+    public static void main(String[] args) {
+        int i = 2;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+
+    }
+
+}
